e675a087f88115d61e1f20a441beafcd3aacf469
l0c0b0b0 <4ndre.m1tsu@protonmail.com>
2026-04-27 21:34:48 -0400

    Update nucleismb.py

--- plugins/nucleismb.py
+++ plugins/nucleismb.py
@@ -17,6 +17,6 @@ class NucleiSmb:
     async def run(target, tag, output, service, protocol, port, module):
             
         """Run nmap-smb scan."""
-        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags smb -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
+        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags smb -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
         
         return await runcommand(cmd=cmd, tag=tag, output=output, module=module)
