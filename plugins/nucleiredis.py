9e451169eddcde0d0cb2447ee94397930ce9503a
l0c0b0b0 <4ndre.m1tsu@protonmail.com>
2026-04-27 21:35:17 -0400

    Update nucleiredis.py

--- plugins/nucleiredis.py
+++ plugins/nucleiredis.py
@@ -18,6 +18,6 @@ class NucleiRedis:
     
             
         """Run nmap-redis scan."""
-        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags redis -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
+        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags redis -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
         
         return await runcommand(cmd=cmd, tag=tag, output=output, module=module)
