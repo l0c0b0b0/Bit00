9287436f51cbefe9af918b68765cb930369fc691
l0c0b0b0 <4ndre.m1tsu@protonmail.com>
2026-04-27 21:33:49 -0400

    Update nucleitftp.py

--- plugins/nucleitftp.py
+++ plugins/nucleitftp.py
@@ -16,6 +16,6 @@ class NucleiTftp:
     async def run(target, tag, output, service, protocol, port, module):
             
         """Run nmap-tftp scan."""
-        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags tftp -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
+        cmd = f"/usr/bin/nuclei -no-color -silent -no-interactsh -target {target} -tags tftp -rate-limit 50 -concurrency 5 -retries 2 -max-host-error 2 -o {output}/scans/{protocol}_{port}_{service}_nuclei.txt"
         
         return await runcommand(cmd=cmd, tag=tag, output=output, module=module)
